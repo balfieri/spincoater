6256b5dca5d26e8d3f6d007a73555b3912cc7716
Robert A Alfieri <balfieri@nvidia.com>
2019-07-17 15:42:41 -0400

    initial

---
+++ main/iot_main.c
@@ -0,0 +1,21 @@
+// Internet of Things App
+//
+// This example code is in the Public Domain.
+//
+// Unless required by applicable law or agreed to in writing, this
+// software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied.
+//-------------------------------------------------------------------
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+
+void app_main()
+{
+    printf("Hello world!\n");
+    fflush(stdout);
+    esp_restart();
+}
