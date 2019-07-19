4e53dd5d343ab1b517576e48ac4b178204e0b73e
Robert A Alfieri <balfieri@nvidia.com>
2019-07-18 23:58:32 -0400

    success in joining

--- wifi.h
+++ wifi.h
@@ -8,10 +8,10 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
+#include "esp_log.h"
 #include "esp_system.h"
 #include "esp_wifi.h"
 #include "esp_event_loop.h"
-#include "esp_log.h"
 
 class WiFi
 {
@@ -29,19 +29,23 @@ private:
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 //
-// IMPLEMENTATION
+// IMesp_event_loop_create_default()PLEMENTATION
 //
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
-static uint32_t           wifi_max_retries = 1;
-static uint32_t           wifi_num_retries = 0;
+static const char *       wifi_ssid;
+static const char *       wifi_password;
+static uint32_t           wifi_max_retries;
+static uint32_t           wifi_num_retries;
 static EventGroupHandle_t wifi_event_group;
 
 void WiFi::init( const char * ssid, const char * password, uint32_t max_retries )
 {
+    wifi_ssid        = ssid;
+    wifi_password    = password;
     wifi_max_retries = max_retries;
     wifi_num_retries = 0;
     wifi_event_group = xEventGroupCreate();
@@ -50,40 +54,53 @@ void WiFi::init( const char * ssid, const char * password, uint32_t max_retries 
     esp_event_loop_init(event_handler, NULL);
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    esp_wifi_init(&cfg);
-    wifi_config_t wifi_config;
-    str_copy8( wifi_config.sta.ssid, ssid, sizeof(wifi_config.sta.ssid) );
-    str_copy8( wifi_config.sta.password, password, sizeof(wifi_config.sta.password) );
-
+    esp_err_t ret = esp_wifi_init( &cfg );
+    if ( ret != ESP_OK ) {
+        ESP_LOGE( "WiFi", "Could not initialize WiFi system: %s", esp_err_to_name( ret ) );
+    }
+    
     ESP_LOGI("WiFi", "Joining %s...", ssid );
     esp_wifi_set_mode(WIFI_MODE_STA);
-    esp_wifi_set_config(WIFI_IF_STA, &wifi_config);
-    if ( !esp_wifi_start() ) {
-        ESP_LOGE( "WiFi", "Could not join %s", ssid );
+    ret = esp_wifi_start();
+    if ( ret != ESP_OK ) {
+        ESP_LOGE( "WiFi", "Could not join %s: %s", ssid, esp_err_to_name( ret ) );
     }
 }
 
 esp_err_t WiFi::event_handler( void *ctx, system_event_t *event )
 {
-    switch(event->event_id) {
-    case SYSTEM_EVENT_STA_START:
-        esp_wifi_connect();
-        break;
-    case SYSTEM_EVENT_STA_GOT_IP:
-        wifi_num_retries = 0;
-        xEventGroupSetBits(wifi_event_group, BIT0);
-        break;
-    case SYSTEM_EVENT_STA_DISCONNECTED:
-        ESP_LOGI( "WiFi", "connection to AP failed" );
-        if ( wifi_num_retries < wifi_max_retries ) {
+    switch(event->event_id) 
+    {
+        case SYSTEM_EVENT_STA_START:
+        {
+            esp_wifi_disconnect();
+            static wifi_config_t wifi_config;
+            str_copy8( wifi_config.sta.ssid, wifi_ssid, sizeof(wifi_config.sta.ssid) );
+            str_copy8( wifi_config.sta.password, wifi_password, sizeof(wifi_config.sta.password) );
+            esp_wifi_set_config( ESP_IF_WIFI_STA, &wifi_config );
             esp_wifi_connect();
-            xEventGroupClearBits(wifi_event_group, BIT0);
-            wifi_num_retries++;
-            ESP_LOGI( "WiFi", "retried connection to AP" );
+            break;
         }
-        break;
-    default:
-        break;
+
+        case SYSTEM_EVENT_STA_GOT_IP:
+            wifi_num_retries = 0;
+            xEventGroupSetBits(wifi_event_group, BIT0);
+            break;
+
+        case SYSTEM_EVENT_STA_DISCONNECTED:
+        {
+            ESP_LOGE( "WiFi", "connection to AP failed" );
+            if ( wifi_num_retries < wifi_max_retries ) {
+                esp_wifi_connect();
+                xEventGroupClearBits(wifi_event_group, BIT0);
+                wifi_num_retries++;
+                ESP_LOGI( "WiFi", "retried connection to AP" );
+            }
+            break;
+        }
+
+        default:
+            break;
     }
     return ESP_OK;
 }
